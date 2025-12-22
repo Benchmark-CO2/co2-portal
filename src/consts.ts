70fa29bfd9048c470288a1ba2a1d431968259c57
Felipe Okino <felipe1okino@live.com>
2025-12-22 15:55:32 -0300

    feat: add favico, change header title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'BIPE';
+export const SITE_TITLE = 'BIPC 📈 | CO₂ Benchmark';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const FRONT_URL = 'https://bipc.org.br/benchmark'
