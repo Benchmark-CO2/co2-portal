48aad022ee4c3d10ddbd1104670fcbd7c9410fb2
mateusscm <mateus.miranda@hotmail.com.br>
2026-04-14 17:14:44 -0300

    fix: correct site title and description formatting in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'BIPC - Portal';
-export const SITE_DESCRIPTION = 'Portal Benchmark Iterativo para Projetos de Baixo Carbono';
-export const FRONT_URL = 'https://app.bipc.org.br/benchmark'
+export const SITE_TITLE = "BIPC";
+export const SITE_DESCRIPTION =
+  "Benchmark Iterativo para Projetos de Baixo Carbono";
+export const FRONT_URL = "https://app.bipc.org.br/benchmark";
